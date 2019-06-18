1b27132c176d3365c7ddeddf160b0fb5beceae46
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-18 10:20:36 +0300

    added new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './components/Posts';
+
 function App() {
   return (
     <div className="App">
@@ -10,14 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <Posts />
       </header>
     </div>
   );
