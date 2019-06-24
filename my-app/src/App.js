509534345ddf18ab0908fcde860cefa75701519b
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-24 11:37:34 +0300

    added reducers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import Post_form from './components/Post_form';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Post_form />
-        <hr />
-        <Posts />
-      </header>
-    </div>
+          <Post_form />
+          <hr />
+          <Posts />
+        </header>
+      </div>
+    </Provider>
   );
 }
-
+// 37324987
 export default App;
